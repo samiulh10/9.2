c8904aae190ab108eca3132c925484e60b7e77c7
samiulh10 <samiulh10@nycstudents.net>
2024-03-12 16:06:51 -0400

    9.7

--- Main.java
+++ Main.java
@@ -31,6 +31,7 @@ public class Main {
 
         System.out.println(cat.equals(testCase));
         System.out.println(cat.toString());
+        System.out.println(testCase.toString());
 
         c.makeNoise();
 
